eddc4e77b62ec09bb1bfa01f5f41f1d6c6b38d7b
Jonas <jonas_biedermann@hotmail.com>
2017-06-11 17:29:24 +0200

    Service Impl Generation works now very safe with Javapoet

--- MicroNet.Tools.Annotations.Processors/src/main/java/micronet/tools/annotation/codegen/CodegenConstants.java
+++ MicroNet.Tools.Annotations.Processors/src/main/java/micronet/tools/annotation/codegen/CodegenConstants.java
@@ -5,4 +5,5 @@ public class CodegenConstants {
 	public static final String MESSAGE_PARAMETER = "MessageParameter";
 	public static final String REQUEST_PARAMETERS = "RequestParameters";
 	public static final String RESPONSE_PARAMETERS = "ResponseParameters";
+	public static final String SERVICE_IMPL_CLASSNAME = "ServiceImpl";
 }
--- MicroNet.Tools.Annotations.Processors/src/main/java/micronet/tools/annotation/codegen/ServiceImplGenerator.java
+++ MicroNet.Tools.Annotations.Processors/src/main/java/micronet/tools/annotation/codegen/ServiceImplGenerator.java
@@ -1,5 +1,7 @@
 package micronet.tools.annotation.codegen;
 
+import static micronet.tools.annotation.codegen.CodegenConstants.*;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,10 +14,26 @@ import java.util.regex.Pattern;
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
 import javax.lang.model.element.Element;
-import javax.tools.JavaFileObject;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.type.TypeMirror;
 import javax.tools.Diagnostic.Kind;
+import javax.tools.JavaFileObject;
+
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.CodeBlock;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterSpec;
+import com.squareup.javapoet.ParameterizedTypeName;
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.TypeSpec;
+import com.squareup.javapoet.TypeVariableName;
 
 import micronet.annotation.MessageListener;
+import micronet.network.Context;
+import micronet.network.IPeer;
+import micronet.network.Request;
+import micronet.network.factory.PeerFactory;
 import micronet.tools.annotation.ServiceAnnotationProcessorContext;
 import micronet.tools.annotation.ServiceDescription;
 
@@ -28,8 +46,111 @@ public class ServiceImplGenerator {
 		this.filer = filer;
 		this.messager = messager;
 	}
+	
+	public void generateServiceImplementation(ServiceDescription serviceDescription) {
+		
+		String serviceName = serviceDescription.getService().getSimpleName().toString();
+		String serviceUri = serviceDescription.getURI();
+		
+		String peerVariableName = "peer";
+		String contextVariableName = "context";
+		String serviceVariableName = "service";
+		String requestVariableName = "request";
+		
+		TypeName serviceTypeName = ClassName.get(serviceDescription.getPackage(), serviceDescription.getName());
+		
+		CodeBlock.Builder startCodeBuilder = CodeBlock.builder()
+				.addStatement("$T.out.println($S)", System.class, serviceName + " started...");
+		for (Element method : serviceDescription.getStartMethods()) {
+			startCodeBuilder.addStatement("$N.$N($N)", serviceVariableName, method.getSimpleName(), contextVariableName);
+		}
+		
+		CodeBlock.Builder stopCodeBuilder = CodeBlock.builder()
+				.addStatement("$T.out.println($S)", System.class, serviceName + " stopped...");
+		for (Element method : serviceDescription.getStopMethods()) {
+			stopCodeBuilder.addStatement("$N.$N($N)", serviceVariableName, method.getSimpleName(), contextVariableName);
+		}
+		
+		
+		CodeBlock.Builder listerCodeBuilder = CodeBlock.builder()
+				.addStatement("$T.out.println($S)", System.class, "Registering message listeners...");
+		
+		for (Element method : serviceDescription.getMessageListeners()) {
+			MessageListener annotation = method.getAnnotation(MessageListener.class);
+			
+			//REF: peer.listen("/register", (Request request) -> service.onRegister(context, request));
+			listerCodeBuilder.addStatement("$N.listen($S, ($T $N) -> $N.$N($N, $N))", 
+					peerVariableName, annotation.uri(), Request.class, requestVariableName, serviceVariableName, method.getSimpleName(), contextVariableName, requestVariableName);
+		}
+		
+		CodeBlock startCode = startCodeBuilder.build();
+		CodeBlock stopCode = stopCodeBuilder.build();
+		CodeBlock listenerCode = listerCodeBuilder.build();
+		
+		TypeSpec stopThread = TypeSpec.anonymousClassBuilder("")
+			    .addSuperinterface(Thread.class)
+			    .addMethod(MethodSpec.methodBuilder("run")
+			        .addAnnotation(Override.class)
+			        .addModifiers(Modifier.PUBLIC)
+			        .addCode(stopCode)
+			        .returns(void.class)
+			        .build())
+			    .build();
+		
+		CodeBlock tryBlock = CodeBlock.builder()
+				.beginControlFlow("try")
+				.addStatement("$T.out.println($S)", System.class,  "Starting " + serviceName + "...")
+				.add("\n")
+				
+				.addStatement("$T $N = $T.createPeer()", IPeer.class, peerVariableName, PeerFactory.class)
+				.addStatement("$T $N = new $T($N, $S)", Context.class, contextVariableName, Context.class, peerVariableName, serviceUri)
+				.addStatement("$T $N = new $T()", serviceTypeName, serviceVariableName, serviceTypeName)
+				.add("\n")
+				
+				.add(listenerCode)
+				.add("\n")
+				
+				.add(startCode)
+				.add("\n")
+				
+				.addStatement("$T.getRuntime().addShutdownHook($L)", Runtime.class, stopThread)
+				
+				.endControlFlow()
+				
+				.beginControlFlow("catch ($T e)", Exception.class)
+				.addStatement("$T.err.println($S)", System.class, "Could not start " + serviceName + "...")
+				.addStatement("e.printStackTrace()")
+				.endControlFlow()
+				.build();
 
-	public void generateServiceImplementation(ServiceDescription description) {
+		
+		
+		MethodSpec main = MethodSpec.methodBuilder("main")
+			    .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+			    .returns(void.class)
+			    .addParameter(String[].class, "args")
+			    .addCode(tryBlock)
+			    .build();
+
+		TypeSpec serviceImplType = TypeSpec.classBuilder(SERVICE_IMPL_CLASSNAME)
+		    .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+		    .addMethod(main)
+		    .build();
+
+		JavaFile javaFile = JavaFile.builder("", serviceImplType).build();
+		System.out.println(javaFile);
+		try {
+			JavaFileObject file = filer.createSourceFile(SERVICE_IMPL_CLASSNAME, serviceDescription.getService());
+			Writer writer = file.openWriter();
+			javaFile.writeTo(writer);
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void generateServiceImplementationOld(ServiceDescription description) {
 		log(description.getService(), "Generating Service implementation: " + description.getService().getSimpleName());
 
 		String additionalImports = "import " + description.getService().toString() + ";\n\n";
