eaa85448fb79440f0e78d6e298898e8adc09f404
Jonas <jonas_biedermann@hotmail.com>
2017-06-15 21:51:43 +0200

    Init with entities

--- MicroNet.Tools.Core/resources/couchbase/init_couchbase.sh
+++ MicroNet.Tools.Core/resources/couchbase/init_couchbase.sh
@@ -7,7 +7,7 @@ couchbase-cli cluster-init \
 	--cluster-username=Administrator \
 	--cluster-password=password \
 	--services=data,index,query,fts \
-	--cluster-ramsize=300 \
+	--cluster-ramsize=500 \
 	--cluster-index-ramsize=256 \
 	--cluster-fts-ramsize=256 \
 	--index-storage-setting=memopt
@@ -26,6 +26,15 @@ couchbase-cli bucket-create -c 127.0.0.1:8091 \
 	--bucket=instance_connections \
 	--bucket-type=couchbase \
 	--bucket-ramsize=100 \
+	--bucket-replica=1 \
+	--bucket-priority=high \
+	--bucket-eviction-policy=valueOnly \
+	-u Administrator -p password
+	
+couchbase-cli bucket-create -c 127.0.0.1:8091 \
+	--bucket=entities \
+	--bucket-type=couchbase \
+	--bucket-ramsize=300 \
 	--bucket-replica=1 \
 	--bucket-priority=high \
 	--bucket-eviction-policy=valueOnly \
