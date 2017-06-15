fd0fbd269e915f806ce5cff3bc2522dc719794eb
JonasMSI <jonas.biedermann@gmail.com>
2017-06-15 19:54:18 +0200

    Preparation for Docker Toolbox

--- MicroNet.Tools.Launch/src/micronet/tools/launch/utility/BuildServiceContainerUtility.java
+++ MicroNet.Tools.Launch/src/micronet/tools/launch/utility/BuildServiceContainerUtility.java
@@ -1,10 +1,15 @@
 package micronet.tools.launch.utility;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.core.runtime.IPath;
 
@@ -23,7 +28,7 @@ public final class BuildServiceContainerUtility {
 
 		IPath projectLocation = serviceProject.getProject().getLocation();
 		String projectPath = projectLocation.toOSString();
-
+		
 		List<String> argArray = new ArrayList<>();
 		argArray.add("docker");
 		argArray.add("build");	
@@ -47,5 +52,89 @@ public final class BuildServiceContainerUtility {
 			e.printStackTrace();
 			return null;
 		}
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		List<String> argArray = new ArrayList<>();
+		argArray.add("docker");
+		argArray.add("image");	
+		argArray.add("ls");
+		
+		ProcessBuilder builder = new ProcessBuilder(argArray);
+		builder.directory(new File("C:\\Program Files\\Docker Toolbox"));
+		builder.redirectErrorStream(true);
+
+		Map<String, String> dockerMachineEnvironmentVariables = getDockerMachineEnvironmentVariables();
+
+		for (Map.Entry<String, String> envVar : dockerMachineEnvironmentVariables.entrySet()) {
+			builder.environment().put(envVar.getKey(), envVar.getValue());
+		}
+		
+		try {
+			Process process = builder.start();
+			printStream(process.getInputStream(), System.out);
+		} catch (IOException e) {
+		
+		}
+	}
+
+	private static Map<String, String> getDockerMachineEnvironmentVariables() {
+		
+		
+		List<String> argArray = new ArrayList<>();
+		argArray.add("C:\\Program Files\\Docker Toolbox\\docker-machine");
+		argArray.add("env");	
+		
+		ProcessBuilder builder = new ProcessBuilder(argArray);
+		
+		try {
+			Process process = builder.start();
+			Map<String, String> envVariables = parseEnvVariables(process.getInputStream());
+			System.out.print(envVariables);
+			return envVariables;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static Map<String, String> parseEnvVariables(InputStream inStream) {
+		Map<String, String> result = new HashMap<>();
+
+		BufferedReader input = new BufferedReader(new InputStreamReader(inStream));
+		String line = null;
+		try {
+			while ((line = input.readLine()) != null) {
+				if (line.contains("REM") || !line.contains("DOCKER"))
+					continue;
+				
+				String[] tokens = line.split("=");
+				
+				int keyStart = tokens[0].indexOf("DOCKER");
+				result.put(tokens[0].substring(keyStart), tokens[1]);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	public static void printStream(InputStream inStream, PrintStream outStream) {
+		new Thread() {
+			public void run() {
+				BufferedReader input = new BufferedReader(new InputStreamReader(inStream));
+				String line = null;
+				try {
+					while ((line = input.readLine()) != null) {
+						outStream.println(line);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}.start();
 	}
 }
